5cda74498233d8d21ed10a217f903e5deb000b32
Matteo Parrucci <parruc@gmail.com>
2019-05-25 16:27:39 +0200

    modified cropping/resizing policy

--- investments/imagegenerators.py
+++ investments/imagegenerators.py
@@ -1,18 +1,19 @@
 from imagekit import ImageSpec
 from imagekit import register
 from imagekit.processors import Thumbnail
+from pilkit.processors import Anchor
 
 
 class InvestmentCardThumbnail(ImageSpec):
-    processors = [Thumbnail(400, 400)]
+    processors = [Thumbnail(999, 400, crop=False, upscale=False)]
     format = 'JPEG'
-    options = {'quality': 60}
+    options = {'quality': 75}
 
 
 class InvestmentDetailThumbnail(ImageSpec):
-    processors = [Thumbnail(400, 999)]
+    processors = [Thumbnail(400, 999, crop=False, upscale=False)]
     format = 'JPEG'
-    options = {'quality': 60}
+    options = {'quality': 75}
 
 
 register.generator('constriction.investments:card', InvestmentCardThumbnail)
