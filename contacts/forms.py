bf8191a5f41a6c6eb8e7135bb32b9f285ee72cd4
Matteo Parrucci <parruc@gmail.com>
2019-05-28 02:00:20 +0200

    added contacts app and few pages

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,7 +1,24 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={'placeholder': _('Come ti chiami?'),
+                   'class': 'w-100'}
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'placeholder': _('La tua mail?'),
+                   'class': 'w-100'}
+        )
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': _('Cosa vuoi dirci?'),
+                   'rows': 4}
+        )
+    )
