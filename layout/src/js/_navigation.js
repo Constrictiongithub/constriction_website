d90620c88bc5c5ab654bde9fd9a0d23eb05c2d32
Matteo Parrucci <parruc@gmail.com>
2019-06-07 23:15:17 +0200

    nuovo logo, footer, rifatta la logica del menu, cambiata ed ingrandita CTA

--- layout/src/js/_navigation.js
+++ layout/src/js/_navigation.js
@@ -6,44 +6,40 @@ $(function () {
     navMain.on("click", "a:not([data-toggle])", null, function () {
         navMain.collapse('hide');
     });
-    
+    // Set active element on load based on hash
+    function setActiveMenuItem(){
+        $('#main-menu a[href*="#"]').removeClass("active");
+        if (window.location.hash) {
+            $('#main-menu a[href*="' + window.location.hash + '"]').addClass("active");
+        } else {
+            $('#main-menu a[href*="#hp"]').addClass("active");
+        }
+    }
+    setActiveMenuItem();
+
     // SMOOTH SCROLLING
-    $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
+    $('a[href*="#"]').click(function (event) {
         // On-page links
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-            &&
-            location.hostname == this.hostname
-        ) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
+            var $target = $(this.hash);
+            if ($target.length) {
                 // Only prevent default if animation is actually gonna happen
                 event.preventDefault();
                 var $navbar_fixed = $("body .navbar.fixed-top");
                 var navbarOffset = $navbar_fixed.length && $navbar_fixed.height() || 0;
-                console.log(target.offset().top);
-                console.log(target.offset().top - navbarOffset);
                 $('html, body').animate({
-                    scrollTop: (target.offset().top - navbarOffset)
+                    scrollTop: ($target.offset().top - navbarOffset)
                 }, 1000, function () {
                     // Callback after animation
                     // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
+                    var hash = "#" + $target.attr("id");
+                    if (history.pushState) {
+                        history.pushState(null, null, hash);
                     } else {
-                        // Had to comment line below to make it work
-                        //$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    };
+                        location.hash = hash;
+                    }
+                    setActiveMenuItem();
                 });
             }
         }
