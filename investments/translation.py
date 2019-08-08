d4178992e982124b98a2e924fc6d4a277e14dcda
Matteo Parrucci <parruc@gmail.com>
2019-08-09 01:29:15 +0200

    added other investments to translate

--- investments/translation.py
+++ investments/translation.py
@@ -1,6 +1,8 @@
 from modeltranslation.translator import TranslationOptions, translator
 
-from investments.models import Business, Investment, P2PLending, RealEstate
+from investments.models import (Bond, Business, Commodity, Equity, HedgeFund,
+                                Investment, P2PLending, PreciousObject,
+                                RealEstate)
 
 
 class InvestmentTranslationOptions(TranslationOptions):
@@ -19,7 +21,32 @@ class BusinessTranslationOptions(TranslationOptions):
     fields = ()
 
 
+class PreciousObjectTranslationOptions(TranslationOptions):
+    fields = ()
+
+class HedgeFundTranslationOptions(TranslationOptions):
+    fields = ()
+
+
+class BondTranslationOptions(TranslationOptions):
+    fields = ()
+
+
+class CommodityTranslationOptions(TranslationOptions):
+    fields = ()
+
+
+class EquityTranslationOptions(TranslationOptions):
+    fields = ()
+
+
+
 translator.register(Investment, InvestmentTranslationOptions)
 translator.register(P2PLending, P2PLendingTranslationOptions)
 translator.register(RealEstate, RealEstateTranslationOptions)
 translator.register(Business, BusinessTranslationOptions)
+translator.register(PreciousObject, PreciousObjectTranslationOptions)
+translator.register(HedgeFund, HedgeFundTranslationOptions)
+translator.register(Bond, BondTranslationOptions)
+translator.register(Commodity, CommodityTranslationOptions)
+translator.register(Equity, EquityTranslationOptions)
